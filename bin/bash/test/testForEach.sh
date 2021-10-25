f5f58e6dfe01230a8bafef4dde59e85be35ce3a1
Remi Pelletier <remi.pelletier@gmail.com>
2021-10-25 14:44:03 -0400

    Added Debugging, removed DOS line endings

--- bin/bash/test/testForEach.sh
+++ bin/bash/test/testForEach.sh
@@ -6,39 +6,65 @@
 ###     see the elements as they happen
 ###
 ### In this case if there are lots of email addresses to add
-###    then an array can be used to load them
+###    then a map/array can be used to load them
 ###
 #############################################################################
 
-set ECHO = false
+ECHO=true
+DEBUG="ON"
 
 #--
-#-- Build the Logging DISTRIB_LIST
-#--   Note: Only the person's eid is needed
+#-- Build a map for the Logging DISTRIB_LIST
 #--
-     recip[0]="developer_001"
-     recip[1]="developer_001"
+recip[0]="developer_001"
+recip[1]="developer_002"
 
-     i=0
-     for email in ${recip[*]}; do
-      
-         if ( "${ECHO}" == "true" ) then
-             print "\$i ........... [${i}]"
-             print "\$email ....... [${email}]"
-         fi
+if [ ${DEBUG}="ON" ]; then
+	printf "\t DEBUG: STARTING \n"
+	printf "\t\t DEBUG ... [${DEBUG}] \n"
+	printf "\t\t ECHO .... [${ECHO}]  \n"
+	printf "\n"
+fi
 
-         if [ `expr ${i}` -eq 0 ]; then
-             DISTRIB_LIST="${email}@company.com"
-         else
-             DISTRIB_LIST="${DISTRIB_LIST},${email}@company.com"
-         fi
- 
-         i=`expr ${i} + 1`
-     done
-        
-     export DISTRIB_LIST
-     
-     if ( "${ECHO}" == "true" ) then
-         print "\$DISTRIB_LIST ... [${DISTRIB_LIST}]"
-     fi
-     
+
+### The ECHO (boolean) CAN BE EVALUATED IN SEVERAL WAYS
+### AS BOOLEAN, EXPLICITLY:
+if [ ${ECHO} == true ]; then
+	printf "\t\t ECHO2 .... [${ECHO}]  \n"
+fi
+### AS BOOLEAN, PLAIN & SIMPLE:
+if [ ${ECHO} ]; then
+	printf "\t\t ECHO3 .... [${ECHO}]  \n"
+fi
+
+printf "\n"
+
+i=0
+for email in ${recip[*]}; do
+
+	if [ ${ECHO} ]; then
+		printf "\t \$i ........... [${i}] \n"
+		printf "\t \$email ....... [${email}] \n"
+		printf "\n"
+	fi
+
+	###
+	### CREATE THE DISTRIBUTION LIST/ARRAY/MAP
+	###
+	### IF THIS IS THE FIRST ITERATION
+	if [ `expr ${i}` -eq 0 ]; then
+		DISTRIB_LIST="${email}@company.com"
+	else
+		DISTRIB_LIST="${DISTRIB_LIST},${email}@company.com"
+	fi
+
+	### INCREMENT the 'i' (iteration) counter
+	i=`expr ${i} + 1`
+done
+
+export DISTRIB_LIST
+
+if [ "${ECHO}" == "true" ]; then
+	printf "\t INFO: \$DISTRIB_LIST ... [${DISTRIB_LIST}]"
+	printf "\n"
+fi
